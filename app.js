df03e4144b3f9c2d743cd61cdf07ed63c5db892a
varun-2024 <v.talwar2024@gmail.com>
2025-03-20 15:15:14 +0000

    Dom Manipulation

---
+++ app.js
@@ -0,0 +1,38 @@
+/* let smallImg = document.getElementsByClassName("oldImg");
+for (let i = 0; i < smallImg.length; i++) {
+  smallImg[i].src = "/assets/assets/spiderman_img.png";
+  console.log("Value Changed of index " + i);
+} */
+/*----------------------------------------------------------/*
+let tagP = document.getElementsByTagName("p");
+for (let i = 0; i < tagP.length; i++) {
+  tagP[i].style.color = "red";
+  console.log("Color Changed of index " + i);
+} */
+
+/*----------------------------------------------------------*/
+
+/* let select = document.querySelectorAll("p");
+
+select.forEach((item, index) => {
+  item.style.color = "red";
+  console.log("Color Changed of index " + index);
+}); */
+
+/*----------------------------------------------------------*/
+/* let select = document.querySelectorAll("img");
+
+select.forEach((item, index) => {
+  item.style.border = "5px solid red";
+  console.log("Color Changed of index " + index);
+}); */
+
+/*----------------------------------------------------------*/
+/* let select = document.querySelectorAll("#mainImg");
+
+select.forEach((item, index) => {
+  item.src = "/assets/assets/creation_1.png";
+  console.log("Color Changed of index " + index);
+}); */
+
+/*----------------------------------------------------------*/
